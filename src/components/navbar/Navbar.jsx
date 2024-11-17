af1bf9c5a8f9b089aacdabeec96abec1e3e184d1
satya kadumula <kadumulasatya@gmail.com>
2024-11-17 18:27:27 +0530

    few modifications in navbar and herosection mobile screen

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,43 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import nikeImg from '../../assets/logo.png'
-import './Navbar.css'
+import nikeImg from '../../assets/logo.png';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(()=>{
+    const ul = document.querySelector('.menu ul');
+    if(menuOpen){
+      ul.classList.add('active');
+      ul.style.display = 'flex';
+    }else{
+      ul.classList.remove('active');
+    }
+  })
+  
   return (
     <nav>
       <div className="navbar">
-      <div className="logo">
-        <img  src={nikeImg} alt="nike-logo" />
+        <div className="logo">
+          <img src={nikeImg} alt="nike-logo" />
+        </div>
+        <div className={`menu ${menuOpen ? 'active' : ''}`}>
+          <ul>
+            <li>
+              <NavLink className='header_elements activeBtn' to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className='header_elements' to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink className='header_elements' to='/contact'>Contact</NavLink>
+            </li>
+            <li>
+              <NavLink className='header_elements' to='/services'>Services</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="Login-Signup">
+          <button type='button' className='navbarBtn'>
+            <Link to='/login' className="loginbtn">Login</Link>
+          </button>
+          <button type='button' className='navbarBtn'>
+            <Link to='/signup' className="loginbtn">SignUp</Link>
+          </button>
+        </div>
+        <div id="nav-icon" onClick={toggleMenu} className={menuOpen ? 'open' : ''}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="menu">
-        <ul>
-          <li>
-            <NavLink className='header_elements activeBtn' to='/'>Home</NavLink>
-          </li>
-          <li>
-            <NavLink className='header_elements' to='/about'>About</NavLink>
-          </li>
-          <li>
-            <NavLink className='header_elements' to='/contact'>Contact</NavLink>
-          </li>
-          <li>
-            <NavLink className='header_elements' to='/services'>Services</NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="Login-Signup">
-        <button type='button' className='navbarBtn'>
-          <Link to='/login' className="loginbtn">Login</Link>
-        </button>
-        <button type='button' className='navbarBtn'>
-          <Link to='/signup' className="loginbtn">SignUp</Link>
-        </button>
-      </div>
-      </div>      
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
